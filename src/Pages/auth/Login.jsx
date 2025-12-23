0591d483ca7942a8312484e361acebe9f695beaf
Dipendra Jung Karki <dipendrajungkarki@Dipendra-ka-MacBook-Pro.local>
2025-12-23 14:44:59 +0545

    feat: Implemented apis for login and register

--- src/Pages/auth/Login.jsx
+++ src/Pages/auth/Login.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import Form from './components/Form'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export const Login = () => {
+
+    const navigate = useNavigate()
+
+    const submitLogin = async (data) => {
+    try {
+      const response = await axios.post('https://react30.onrender.com/api/user/login', data)
+      if(response?.status == 200){
+        navigate('/')
+      }
+      else{
+        alert(`Something went wrong!, ${response.status}`)
+      }
+    } catch (error) {
+      alert(error?.response?.data?.message)
+    }
+  }
+
   return (
-    <Form type="login"/>
+    <Form type="login" onSubmit={submitLogin} />
   )
 }
--- src/Pages/auth/Register.jsx
+++ src/Pages/auth/Register.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import Form from './components/Form'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export const Register = () => {
+  const navigate = useNavigate()
+
+  const submitRegister = async (data) => {
+    try {
+      const response = await axios.post('https://react30.onrender.com/api/user/register', data)
+      if(response?.status == 201){
+        navigate('/login')
+      }
+      else{
+        alert(`Something went wrong!, ${response.status}`)
+      }
+    } catch (error) {
+      alert(error?.response?.data?.message)
+    }
+  }
+
   return (
-    <Form type="register"/>
+    <Form type="register" onSubmit={submitRegister} />
   )
 }
